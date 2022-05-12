c376fe528b48cc248dd2065035bb61975fd599de
igor.shtrotkin <=>
2022-05-12 17:34:07 +0400

    Добавлен класс контекста EF

---
+++ ToDo.DAL/EF/EFToDoContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.DAL.EF
+{
+    public class EFToDoContext : DbContext
+    {
+    }
+}
